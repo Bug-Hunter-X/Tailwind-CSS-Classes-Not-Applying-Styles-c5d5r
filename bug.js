4688d33fee6d7c9ec750896a617925dd17c13f2e
BugHunter-X <askzenai@gmail.com>
2024-12-28 11:24:05 +0100

    Add bug.js file

---
+++ bug.js
@@ -0,0 +1,6 @@
+```javascript
+// Some Tailwind classes are not working as expected.
+<div class="bg-red-500 p-4 rounded-lg">
+  <p>This text should have a red background, padding, and rounded corners.</p>
+</div>
+```
